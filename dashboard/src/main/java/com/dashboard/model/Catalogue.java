10c9fa8b5fd983436168ebd8ee1993fc2618c4f7
Ayan <ayan.sen@live.com>
2017-10-21 02:22:56 +0530

    Added catalogue

--- dashboard/src/main/java/com/dashboard/model/Catalogue.java
+++ dashboard/src/main/java/com/dashboard/model/Catalogue.java
@@ -1,28 +1,41 @@
 package com.dashboard.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 @Entity
-@Table(name="catalogue")
-public class Catalogue {
-	
+@Table(name="CATALOGUE")
+public class Catalogue implements Serializable {  	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="catalogue_id")
 	private String catalogueId;
 	
-	@Column(name="catalogue_id")
+	@Column(name="catalogue_name")
 	private String catagogueName;
-
-	public Catalogue(String catalogueId, String catagogueName) {
+	
+	public Catalogue() {
 		super();
-		this.catalogueId = catalogueId;
+	}
+
+	public Catalogue(String catagogueName) {
+		super();
 		this.catagogueName = catagogueName;
+	}
+
+	public void setCatalogueId(String catalogueId) {
+		this.catalogueId = catalogueId;
 	}
 
 	public String getCatalogueId() {
--- dashboard/src/main/java/com/dashboard/repository/CatalogueRepository.java
+++ dashboard/src/main/java/com/dashboard/repository/CatalogueRepository.java
@@ -2,11 +2,14 @@ package com.dashboard.repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
 import com.dashboard.model.Catalogue;
 
+
+@Transactional
 @Repository
 public class CatalogueRepository {
 
@@ -14,7 +17,14 @@ public class CatalogueRepository {
 	private EntityManager entityManager;	
 	
 	public String add(Catalogue catalogue) {
+		catalogue.setCatalogueId(generateId());
 		entityManager.persist(catalogue);
+		entityManager.flush();
 		return catalogue.getCatalogueId();
+	}
+	
+	private String generateId() {
+		String sql = "SELECT CONCAT('C',LPAD(IFNULL(MAX(RIGHT(CATALOGUE_ID,9)),  0)+1,10-LENGTH(IFNULL(MAX(RIGHT(CATALOGUE_ID,9)),  0)+1),'0')) AS id FROM CATALOGUE";
+		return entityManager.createNativeQuery(sql).getSingleResult().toString();
 	}
 }
